8587b895d310ef83e5f81b8f5f2f71be6ac59f0b
ChaninL <chaninlawlert@gmail.com>
2024-04-08 17:13:35 +0700

    Time: 60 ms, Memory: 52.3 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,12 +1,12 @@
 function removeDuplicates(nums: number[]): number {
     const set = new Set() 
-    let i: number = 0
+    let i = 0
     nums.forEach((num) => {
         if (!set.has(num)) {
-            set.add(num)
-            nums[i++] = num
+          set.add(num)
+          nums[i++] = num
         }
     })
-    console.log(nums)
+  
     return i
 };
