ea082c800e8d699ffdebf7372cd76a407368bbf8
ChaninL <chaninlawlert@gmail.com>
2024-04-08 11:51:34 +0700

    Time: 77 ms, Memory: 52.5 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.ts
@@ -0,0 +1,16 @@
+/**
+ Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            for (let j = arr.length - 1; j > i; j--) {
+                arr[j] = arr[j - 1]
+            }
+            if (i + 1 !== arr.length) {
+                arr[i + 1] = 0
+                i++    
+            }
+        }
+    }
+};
