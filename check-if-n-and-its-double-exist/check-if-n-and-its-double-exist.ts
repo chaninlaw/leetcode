67ebac63c407e8825396247e8a6fadb77b2ce992
ChaninL <chaninlawlert@gmail.com>
2024-04-09 01:36:23 +0700

    Time: 68 ms, Memory: 52.4 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.ts
@@ -0,0 +1,17 @@
+function checkIfExist(arr: number[]): boolean {
+  const map: Record<string, boolean> = {}
+  
+  for (let i = 0; i < arr.length; i++) {
+     if (map[2 * arr[i]]) {
+       console.log('1', arr[i])
+       return true
+     } else if (map[arr[i] / 2]) {
+       console.log('2', arr[i] / 2)
+       return arr[i] % 2 === 0
+     } else {
+       map[arr[i]] = true
+     }
+  }
+  
+  return false
+};
