a9c87610d268f0b672bdff704df158dd29fd87d5
ChaninL <chaninlawlert@gmail.com>
2024-04-09 21:04:53 +0700

    Time: 459 ms, Memory: 60.6 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.ts
@@ -0,0 +1,31 @@
+function validMountainArray(arr: number[]): boolean {
+  if (arr.length < 3) return false
+  
+  let prev = arr[0]
+  let isUp: boolean | undefined
+  let isDown: boolean | undefined
+  for (let i = 1; i < arr.length; i++) {
+    if (prev === arr[i]) return false
+    
+    const increasing = prev - arr[i] < 0
+    
+    console.log('increasing', increasing, arr[i])
+
+    // first increasing
+    if (increasing && isUp === undefined) {
+      isUp = true
+    }
+    
+    // first decreasing
+    if (!increasing && isUp && isDown === undefined) {
+      isUp = false
+      isDown = true
+    }
+    
+    if (!increasing && !isDown) return false
+    if (increasing && !isUp) return false
+    
+    prev = arr[i]
+  }
+  return !isUp && isDown
+};
