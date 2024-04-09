501dfe3d2e1b7e4f012b1494b1eaf36adb2eacb4
ChaninL <chaninlawlert@gmail.com>
2024-04-09 23:55:51 +0700

    Time: 164 ms, Memory: 59.1 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
@@ -1,22 +1,26 @@
 function replaceElements(arr: number[]): number[] {
-    let max = arr[arr.length - 1]
-    
-    arr[arr.length - 1] = -1
-  
-    for (let i = arr.length - 1; i >= 0; i--) {
-      
-      if (max < arr[i]) {
-        max = arr[i]
-      } else {
-        const temp = arr[i - 1]
-        
-        arr[i - 1] = max
-        
-        if (temp > max) {
-          max = temp
-        }
+  if (arr.length === 1) {
+    arr[0] = -1
+    return arr
+  }
+  let max = arr[arr.length - 1]
+
+  arr[arr.length - 1] = -1
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+
+    if (max < arr[i]) {
+      max = arr[i]
+    } else {
+      const temp = arr[i - 1]
+
+      arr[i - 1] = max
+
+      if (temp > max) {
+        max = temp
       }
     }
+  }
   
   return arr
 };
